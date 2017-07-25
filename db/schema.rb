adaf87aae4c6ce62d7d948cad14763d65fe74ea3
Rika Yoshida <nazuki@genshin.org>
2017-07-25 10:44:42 +0900

    add lock_version to protocol model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170718004554) do
+ActiveRecord::Schema.define(version: 20170725013249) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -73,6 +73,7 @@ ActiveRecord::Schema.define(version: 20170718004554) do
     t.float "version", default: 0.001, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "lock_version", default: 0, null: false
   end
 
   create_table "reviewer_users", force: :cascade do |t|
