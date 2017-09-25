8912921298afba9699f65fdda06207587316b2ad
Rika Yoshida <rika.yoshida@nnh.go.jp>
2017-09-25 10:05:34 +0900

    fix seed

--- db/seeds/section.rb
+++ db/seeds/section.rb
@@ -3,7 +3,9 @@ Dir.glob("#{Rails.root}/db/seeds/section_*.yml") do |filename|
   File.open(filename) do |yml_file|
     records = YAML.load(yml_file)['sections']
     records.each do |record|
-      Section.create(record)
+      section = Section.where(no: record['no'], template_name: record['template_name']).first_or_create
+      section.update_attributes(record)
+      section.save!
     end
   end
 end
