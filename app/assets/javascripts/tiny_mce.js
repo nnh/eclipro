b813c383f51709bfbbf2df1c2e874d44b2a61424
Rika Yoshida <nazuki@genshin.org>
2017-10-05 16:17:19 +0900

    [wip] add tinymce plugin

--- app/assets/javascripts/tiny_mce.js
+++ app/assets/javascripts/tiny_mce.js
@@ -7,7 +7,8 @@ function initTinyMCE(selector) {
       theme_advanced_toolbar_align: "left",
       theme_advanced_statusbar_location: "bottom",
       theme_advanced_buttons3_add: ["tablecontrols","fullscreen"],
-      plugins: "table, fullscreen, lists, advlist, textcolor, emoticons, charmap, image",
+      plugins: "print, paste, searchreplace, media, link, hr, anchor, pagebreak, insertdatetime, nonbreaking, template, toc," +
+               "visualchars, visualblocks, preview, table, fullscreen, lists, advlist, textcolor, emoticons, charmap, image",
       toolbar: [
         "bold italic underline | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | forecolor backcolor emoticons | fullscreen charmap",
         "image reference"
@@ -23,7 +24,7 @@ function initTinyMCE(selector) {
       }
     });
   } else {
-    setTimeout(initTinyMCE, 50);
+    setTimeout(initTinyMCE(selector), 50);
   }
 }
 
