a334147ad14f125d29565b99738b2a666955920e
Rika Yoshida <rika.yoshida@nnh.go.jp>
2018-02-01 17:05:35 +0900

    fix javascript for export

---
+++ app/javascript/packs/export.js
@@ -0,0 +1,16 @@
+const $ = require('jquery');
+
+$(function() {
+  const vars = {};
+  const locations = document.location.search.substring(1).split('&');
+  for (let i in locations) {
+    const location = locations[i].split('=', 2);
+    vars[location[0]] = decodeURIComponent(location[1]);
+  }
+  const classNames = ['frompage', 'topage', 'page', 'webpage', 'section', 'subsection', 'subsubsection'];
+  for (let i in classNames) {
+    const elements = document.getElementsByClassName(classNames[i]);
+    for (let j = 0; j < elements.length; ++j)
+      elements[j].textContent = vars[classNames[i]];
+  }
+});
