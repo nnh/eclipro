5c9205cd521b27d8d8b28bda8394e4e5356da91b
Rika Yoshida <rika.yoshida@nnh.go.jp>
2018-04-23 17:00:11 +0900

    fix migration file

--- db/migrate/20180201024719_delete_section.rb
+++ db/migrate/20180201024719_delete_section.rb
@@ -1,6 +1,6 @@
 class DeleteSection < ActiveRecord::Migration[5.1]
   def change
-    Section.where(no: ['title', 'compliance']).destroy_all
-    Content.where(no: ['title', 'compliance']).destroy_all
+    Section.reorder('').where(no: ['title', 'compliance']).destroy_all
+    Content.reorder('').where(no: ['title', 'compliance']).destroy_all
   end
 end
