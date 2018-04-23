348a0295321654c71a68beedbee273dd2b33ef79
Rika Yoshida <rika.yoshida@nnh.go.jp>
2018-04-23 13:11:35 +0900

    fix seed

--- db/seeds/section.rb
+++ db/seeds/section.rb
@@ -1,15 +1,14 @@
 sanitizer = Rails::Html::WhiteListSanitizer.new
 
 Dir.glob("#{Rails.root}/db/seeds/**/section_*.yml") do |filename|
-  puts filename
   File.open(filename) do |yml_file|
     records = YAML.load(yml_file)['sections']
     records.each do |record|
-      section = Section.where(no: record['no'], template_name: record['template_name']).first_or_create
+      section = Section.where(no: record['no'], seq: record['seq'], template_name: record['template_name']).first_or_create
       record['template'] = sanitizer.sanitize(record['template']).gsub(/\R/, '')
       record['instructions'] = sanitizer.sanitize(record['instructions']).gsub(/\R/, '')
       record['example'] = sanitizer.sanitize(record['example']).gsub(/\R/, '')
-      section.update_attributes!(record)
+      section.update!(record)
     end
   end
 end
