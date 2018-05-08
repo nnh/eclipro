16a2f00f203de35372d86ab9ef5b4eb5ed1a872c
Rika Yoshida <rika.yoshida@nnh.go.jp>
2018-05-08 15:11:52 +0900

    add spec

--- spec/models/content_spec.rb
+++ spec/models/content_spec.rb
@@ -2,11 +2,17 @@ require 'rails_helper'
 
 describe Content do
   let(:protocol) { create(:protocol) }
+  let(:jccg_protocol) { create(:protocol, template_name: 'JCCG') }
   let(:reviewer) { create(:user) }
   let!(:reviewer_participation) { create(:reviewer, protocol: protocol, user: reviewer, sections: [0]) }
 
   context 'has_reviewer?' do
     it { expect(protocol.contents.root.has_reviewer?).to eq(true) }
     it { expect(protocol.contents.find_by(no: 1, seq: 0).has_reviewer?).to eq(false) }
+  end
+
+  context 'return root section' do
+    it { expect(protocol.contents.root.no).to eq(0) }
+    it { expect(jccg_protocol.contents.root).to eq(nil) }
   end
 end
